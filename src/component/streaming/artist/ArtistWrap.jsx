cd355a9711744c437f3800849b24d9fd6a38567a
danbijyj <danbijyj3@gmail.com>
2025-09-07 16:39:17 +0900

    JYJ0907

--- src/component/streaming/artist/ArtistWrap.jsx
+++ src/component/streaming/artist/ArtistWrap.jsx
@@ -1,9 +1,16 @@
+import ArtistCategory from './artistcategory/ArtistCategory';
+import ArtistGoods from './artistgoods/ArtistGoods';
+import RecArtist from './recartist/RecArtist';
 import './style.scss';
 
 const ArtistWrap = () => {
     return (
         <section id="artistwrap">
-            <div className="inner">아티스트메인</div>
+            <div className="inner">
+                <RecArtist />
+                <ArtistCategory />
+                <ArtistGoods />
+            </div>
         </section>
     );
 };
--- src/component/streaming/artist/artistcategory/ArtistCategory.jsx
+++ src/component/streaming/artist/artistcategory/ArtistCategory.jsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import ArtistCategoryItem from './ArtistCategoryItem';
+import './style.scss';
 
 const ArtistCategory = () => {
-    return <div></div>;
+    return (
+        <section id="artist-category">
+            <h2>아티스트별</h2>
+            <div className="artist-category-list">
+                <ArtistCategoryItem />
+                <ArtistCategoryItem />
+                <ArtistCategoryItem />
+                <ArtistCategoryItem />
+                <ArtistCategoryItem />
+                <ArtistCategoryItem />
+                <ArtistCategoryItem />
+                <ArtistCategoryItem />
+                <ArtistCategoryItem />
+                <ArtistCategoryItem />
+                <ArtistCategoryItem />
+                <ArtistCategoryItem />
+                <ArtistCategoryItem />
+                <ArtistCategoryItem />
+                <ArtistCategoryItem />
+            </div>
+        </section>
+    );
 };
 
 export default ArtistCategory;
